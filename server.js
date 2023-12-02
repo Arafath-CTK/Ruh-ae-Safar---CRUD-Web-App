6f54503dc3a193828d91a862aac0c372fe6d1bfd
Arafath <arafathctk@gmail.com>
2023-12-02 14:22:52 +0530

    second commit

--- server.js
+++ server.js
@@ -18,6 +18,7 @@ const server = http.createServer((req, res) => {
       }
     });
   } else if (route.toLocaleLowerCase() === "/submit" && method === "POST") {
+    // we are using this '/submit'route only for the submission of data into the json file. it happens in background.
     let body = ""; //Creating an empty string for storing the incoming value from the client side.
     // in our case, which is already stored in a property called 'body'.
     // the name has no connection with that, taking the same name for easy understandability.
@@ -25,7 +26,7 @@ const server = http.createServer((req, res) => {
     // req.on is a part of http module, it is used to set up event listeners for specific events
     // like 'data' , 'error' , 'end' , etc.
     req.on("data", (chunk) => {
-      //here the incoming value is a data, so we are setting up event listener for data,
+      // here the incoming value is a data, so we are setting up event listener for data,
       // And data is coming as it split into smaller pieces called chunks.
 
       body = body + chunk; //the incoming chunks are assigned to the empty string.
@@ -51,7 +52,7 @@ const server = http.createServer((req, res) => {
             res.writeHead(500, { "Content-Type": "text/plain" });
             res.end("Unexpected error occured while processing data");
           } else {
-            const existingDataArray = []; // dummy array created
+            let existingDataArray = []; // dummy array created
             existingDataArray = JSON.parse(existingData); //The existing from the database added to the dummy array.
             const newId = existingDataArray.length + 1;
             recievedData.id = newId; // the id property being increased for every user.
@@ -85,10 +86,91 @@ const server = http.createServer((req, res) => {
         res.end("Unexpected error occured while processing data");
       }
     });
+  } else if (
+    method === "GET" &&
+    route.toLocaleLowerCase() === "/bookingstable"
+  ) {
+    fs.readFile("table.html", "utf-8", (err, data) => {
+      if (err) {
+        res.writeHead(500, { "Content-Type": "text/html" });
+        res.end("Error loading the table data");
+      } else {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.write(data);
+        res.end();
+      }
+    });
+  } else if (method === "GET" && route.toLocaleLowerCase() === "/getdata") {
+    fs.readFile("data.json", "utf-8", (err, data) => {
+      if (err) {
+        res.writeHead(500, { "Content-Type": "text/html" });
+        res.end("Error loading the data");
+      } else {
+        res.writeHead(200, { "Content-Type": "application/json" });
+        res.end(data);
+      }
+    });
+  } else if (
+    method === "PUT" &&
+    route.toLocaleLowerCase().startsWith === `/modifyuser`
+  ) {
+    const parsedUrl = url.parse(route, true); //parsed url and by specifying "true", it will return an object. easy for further operations.
+    const userId = parseInt(parsedUrl.pathname.split("/").pop());
+
+    let body = "";
+    req.on("data", (chunk) => {
+      body = body + chunk;
+    });
+
+    req.on("end", () => {
+      try {
+        const updatedData = JSON.parse(body);
+
+        fs.readFile("data.json", "utf-8", (err, existingData) => {
+          if (err) {
+            console.error("Error processing data", err);
+            res.writeHead(500, { "Content-Type": "text/plain" });
+            res.end("Unexpected error occured while processing data");
+          } else {
+            let existingDatas = JSON.parse(existingData);
+
+            for (let i = 0; i < existingDatas.length; i++) {
+              if (existingDatas[i].id === userId) {
+                existingDatas[i] = updatedData;
+                break;
+              }
+            }
+
+            fs.writeFile(
+              "data.json",
+              JSON.stringify(existingDatas, null, 2),
+              (err) => {
+                if (err) {
+                  console.error("Error processing data", err);
+                  res.writeHead(500, { "Content-Type": "text/plain" });
+                  res.end(
+                    "Unexpected error occured while updating the database"
+                  );
+                } else {
+                  console.log("Data updated to the database");
+                  res.writeHead(200, { "Content-Type": "text/plain" });
+                  res.end("Data updated to the database successfully");
+                }
+              }
+            );
+          }
+        });
+      } catch (error) {
+        console.error("Error processing data", error);
+        res.writeHead(500, { "Content-Type": "text/plain" });
+        res.end("Unexpected error occured while processing data");
+      }
+    });
   } else {
     res.writeHead(404, { "Content-Type": "text/plain" });
     res.end("Server not found");
   }
 });
+
 const PORT = process.env.PORT || 3000;
 server.listen(3000, () => console.log(`Server running on ${PORT}`));
